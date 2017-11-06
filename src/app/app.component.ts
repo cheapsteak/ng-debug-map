b2d9cc22beaab56491adfc7171ff25986abff655
Chang Wang <garnwraly@gmail.com>
2017-11-05 22:38:57 -0500

    add broken code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <button (click)="onClick()">click</button>
+      {{ map | json }}
+    </div>
+  `,
+  styles: [`
+    
+  `]
 })
 export class AppComponent {
-  title = 'app';
+  map = new Map();
+
+  onClick () {
+    this.map.set({'a':Math.random()}, {[Math.random()]: {'a':Math.random()}})
+  }
 }
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
